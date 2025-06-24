c684af07dcece11efc73612bdce2a50a4cff0741
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-06-24 19:42:19 +0530

    Refine comment on inorder successor in deleteNode function

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -82,7 +82,7 @@ Node* deleteNode(Node* root, int key) {
         }
         // Case 2: Two children
         Node* temp = findMin(root->right); // find the inorder successor
-        // inorder successor = ->  the smallest node in the right subtree and the next larger element
+        // inorder successor = the smallest node in the right subtree and the next larger element
         root->data = temp->data;
         root->right = deleteNode(root->right, temp->data);
     }
